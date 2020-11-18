4ac81e034a6175d7224f5b5c8bfb8c51573036b7
RobertVogue <rcvogtritter@gmail.com>
2020-11-18 16:48:04 -0700

    done

--- 04-execution-time-differences.js
+++ 04-execution-time-differences.js
@@ -1,23 +1,55 @@
 function myMin1a(list) {
-  // Code goes here ...
-}
+  for (let i = 0; i < list.length - 1; i++) {
+    let min = true
+    for (let j = i + 1; j < list.length; j++) {
+      if (list[j] < list[i]) min = false;
 
+    }
+    if (min) return list[i]
+  }
+  return list[list.length - 1]
+}
+// const list = [ 0, 3, 5, 4, -5, 10, 1, 90, -8 ]
+// console.log(myMin1a(list))  // =>  -5
 
 function myMin1b(list) {
-  // Code goes here ...
+  // Skip
 }
 
 
 function myMin2(list) {
-  // Code goes here ...
+  // Skip
 }
 
 
 function largestContiguousSubsum1(array) {
-  // Code goes here ...
+  let sub = new Array();
+  for (let i = 0; i < array.length; i++) {
+    sub.push(array[i])
+    for (let j = i + 1; j < array.length; j++) {
+      let newEl = array.slice(i, j + 1)
+      sub.push(newEl)
+    }
+  }
+  let sub2 = [];
+  for (let key of sub) {
+    sub2.push(key.reduce((a, b) => a + b))
+  }
+  return Math.max(...sub2)
 }
-
+// const array = [2, 3, -6, 7, -6, 7]
+// console.log(largestContiguousSubsum1(array)) // => 8 (from [7, -6, 7])
 
 function largestContiguousSubsum2(array) {
-  // Code goes here ...
+  let ma = array[0]
+  let current = array[0]
+  for (let i = 1; i < array.length; i++){
+    if (current < 0) current = 0;
+    current += array[i];
+    if (current > max) max = current;
+
+
+  } return max;
 }
+const array = [2, 3, -6, 7, -6, 7]
+console.log(largestContiguousSubsum2(array)) // => 8 (from [7, -6, 7])
