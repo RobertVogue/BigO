e074dbe8031a0ad687c6c353307e7ca89e28b6b6
Evanmhargett <evanmhargett@gmail.com>
2020-11-18 17:40:13 -0500

    Two sum implementation

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -12,17 +12,76 @@ function firstAnagram(str1, str2) {
   return (splitString2.length === 0)
 }
 
-console.log(firstAnagram("gizmo", "sally"));    // => false
-console.log(firstAnagram("elvis", "lives"));    // => true
+// console.log(firstAnagram("gizmo", "sally"));    // => false
+// console.log(firstAnagram("elvis", "lives"));    // => true
 
 function secondAnagram(str1, str2) {
-  // Code goes here ....
+  let sorted1 = str1.split('').sort().join('')
+  let sorted2 = str2.split('').sort().join('')
+  return (sorted1 === sorted2)
 }
+
+// console.log(secondAnagram("gizmo", "sally"));    // => false
+// console.log(secondAnagram("elvis", "lives"));    // => true
 
 
 function thirdAnagram(str1, str2) {
-  // Code goes here ....
+  let count1 = {}
+  let count2 = {}
+  str1.split('').forEach((char) => {
+    if(count1[char] === undefined)
+    {
+      count1[char] = 1
+    }
+    else{
+      count1[char] = count1[char] + 1
+    }
+  })
+  str2.split('').forEach((char) => {
+    if(count2[char] === undefined)
+    {
+      count2[char] = 1
+    }
+    else{
+      count2[char] = count2[char] + 1
+    }
+  })
+  // console.log(JSON.stringify(count1))
+  // console.log(JSON.stringify(count2))
+
+  //check that count1 and count2 have same number keys
+  //if not, return false
+  //loop over all keys in count1
+  //for each key, check if count2[key] === value in count1
+  //if not, return false
+  //if it finishes loop return true
+  let keys1 = Object.keys(count1).sort()
+  let keys2 = Object.keys(count2).sort()
+  
+ 
+  
+  if( JSON.stringify(keys1) !== JSON.stringify(keys2))
+  {
+    
+      return false
+  }
+  else{
+    for(key in count1)
+    {
+      if(count1[key] !== count2[key])
+      {
+        
+        return false
+      }
+    }
+  }
+  return true
+
+
 }
+
+// console.log(thirdAnagram("gizmo", "sally"));    // => false
+// console.log(thirdAnagram("elvis", "lives"));    // => true
 
 
 function fourthAnagram(str1, str2) {
--- 03-two-sum.js
+++ 03-two-sum.js
@@ -1,11 +1,75 @@
 function badTwoSum(arr, targetSum) {
-  // Code goes here ...
+  for(let i =0 ; i < arr.length -1 ; i++)
+  {
+    for(let j = i + 1; j < arr.length; j++)
+    {
+      if(arr[i] + arr[j] === targetSum)
+      {
+        return true
+      }
+    }
+  }
+  return false
+}
+// const arr = [0, 1, 5, 7];
+// console.log(badTwoSum(arr, 6)); // => should be true
+// console.log(badTwoSum(arr, 10)); // => should be false
+
+const binarySearch = (nums, targetNum) => {
+  // if nums has no length, return false because we've run out of items to
+  // search and haven't found targetNum
+  if(nums.length === 0)
+  {
+    return false
+  }
+
+  // determine the slice point
+
+  let middle = Math.floor(nums.length / 2)
+
+  // create "left half" and "right half" arrays
+  let left = nums.slice(0, middle)
+  let right = nums.slice(middle + 1)
+
+  // if targetNum is less than the slice point, return this search on the left
+  // half
+  if(targetNum < nums[middle])
+  {
+    return binarySearch(left, targetNum)
+  }
+
+  // if targetNum is greater than the slice point, return this search on the
+  // right half
+  if(targetNum > nums[middle])
+  {
+    return binarySearch(right, targetNum)
+  }
+
+  // if it's not greater than or less than, we know it's equal so return true
+  return true
 }
 
 
 function okayTwoSum1(arr, targetSum) {
   // Code goes here ...
+  arr.sort()
+ 
+  
+  for(let i = 0; i < arr.length; i++){
+  
+    let newTarget = targetSum - arr[i]
+    let truth = binarySearch(arr.slice(0, i).concat(arr.slice(i+1)), newTarget)
+    if(truth)
+    {
+      return true
+    }
+  }
+  return false
 }
+
+// const arr = [0, 1, 5, 7];
+// console.log(okayTwoSum1(arr, 6)); // => should be true
+// console.log(okayTwoSum1(arr, 10)); // => should be false
 
 
 function okayTwoSum2(arr, targetSum) {
@@ -14,9 +78,24 @@ function okayTwoSum2(arr, targetSum) {
 
 
 function twoSum(arr, targetSum) {
-  // Code goes here ...
+  let obj = {}
+  for(let key of arr)
+  {
+    let value = targetSum - key
+    if(obj[value])
+    {
+      return true
+    }
+    
+    obj[key] = true
+  }
+  return false
 }
 
+
+const arr = [0, 1, 5, 7];
+console.log(twoSum(arr, 6)); // => should be true
+console.log(twoSum(arr, 10)); // => should be false
 
 function twoSumIndices(arr, targetSum) {
   // Code goes here ...
